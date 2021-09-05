71c171b9c9db76b0e1f8590db9b4db0a846ebd7e
donghyeonho <ehdgus1128@gmail.com>
2021-09-06 00:18:28 +0900

    [donghyeonho_20210906] 1. update file

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,13 @@ const clock = document.querySelector("span#clock");
 
 function getClock() {
   const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,7 @@
 const API_KEY = '85f48ef4e350105274b2461976317964';
 
 function onGeoOk(position){
+    document.querySelector('div#weather').classList.remove('hidden');
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
@@ -8,7 +9,7 @@ function onGeoOk(position){
         const weatherContainer = document.querySelector('#weather span:first-child');
         const cityContainer = document.querySelector('#weather span:last-child');
         cityContainer.innerText = data.name;
-        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp} ℃`;
     });
 }
 
